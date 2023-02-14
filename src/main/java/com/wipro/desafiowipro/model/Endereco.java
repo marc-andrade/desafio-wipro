a93facf5cfba29c05e0b6ab3cbc21c66648a7245
Marcos Andrade <marcos_q_andrade@hotmail.com>
2023-02-14 18:50:42 -0300

    Entidade criada

--- src/main/java/com/wipro/desafiowipro/model/Endereco.java
+++ src/main/java/com/wipro/desafiowipro/model/Endereco.java
@@ -1,5 +1,84 @@
 package com.wipro.desafiowipro.model;
 
 
+import java.math.BigDecimal;
+
 public class Endereco {
+
+    private String cep;
+    private String rua;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private BigDecimal frete;
+
+    public Endereco() {
+    }
+
+    public Endereco(String cep, String rua, String complemento, String bairro, String cidade, String estado, BigDecimal frete) {
+        this.cep = cep;
+        this.rua = rua;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.frete = frete;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public BigDecimal getFrete() {
+        return frete;
+    }
+
+    public void setFrete(BigDecimal frete) {
+        this.frete = frete;
+    }
 }
