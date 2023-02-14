ec47a150f18a28953c5f5f8291a3e5470497fe19
Marcos Andrade <marcos_q_andrade@hotmail.com>
2023-02-14 18:46:01 -0300

    projeto criado

---
+++ src/main/java/com/wipro/desafiowipro/DesafioWiproApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.desafiowipro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioWiproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioWiproApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/wipro/desafiowipro/model/Endereco.java
@@ -0,0 +1,5 @@
+package com.wipro.desafiowipro.model;
+
+
+public class Endereco {
+}
